dfe6673cd09d59f4997c7a85dec1764711ac5349
monkey <iamsamspike@gmail.com>
2019-08-02 21:48:29 +1000

    hopful fix of no avatar problem

--- public/javascripts/add_avatar.js
+++ public/javascripts/add_avatar.js
@@ -7,19 +7,19 @@ var avatar_button_blue = document.querySelector(".avatar_button_blue")
 var avatar_button_red = document.querySelector(".avatar_button_red")
 
 var clickWizardDogGreen = function(){
-    avatar.value = "assets/dog_wizard_green.png"
+    avatar.value = "assets/dog_wizard_green_big.png"
     avatar_button_green.classList.add('clicked')
     avatar_button_red.classList.remove('clicked')
     avatar_button_blue.classList.remove('clicked')
 }
 var clickWizardDogRed = function(){
-    avatar.value = "assets/dog_wizard_red.png"
+    avatar.value = "assets/dog_wizard_red_big.png"
     avatar_button_red.classList.add('clicked')
     avatar_button_green.classList.remove('clicked')
     avatar_button_blue.classList.remove('clicked')
 }
 var clickWizardDogBlue = function(){
-    avatar.value = "assets/dog_wizard_blue.png"
+    avatar.value = "assets/dog_wizard_blue_big.png"
     avatar_button_blue.classList.add('clicked')
     avatar_button_green.classList.remove('clicked')
     avatar_button_red.classList.remove('clicked')
